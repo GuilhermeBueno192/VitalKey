25e19054de08cd422c2d8a9a1a5ca37e0dcb6293
Guilherme Bueno <guilhermebueno816@gmail.com>
2025-12-04 22:07:15 -0300

    Corrigindo erro no token

--- app/security/auth.py
+++ app/security/auth.py
@@ -15,7 +15,7 @@ def criar_token(dados: dict):
     expiracao = datetime.now(timezone.utc) + timedelta(minutes=TEMPO_EXPIRACAO)
     dados_copia.update({
         "exp": int(expiracao.timestamp()),  # data de expiração
-        "sub": dados_copia.get("id")       # identificador do usuário
+        "sub": dados_copia.get("sub")       # identificador do usuário
     })
     token_jwt = jwt.encode(dados_copia, SECRET_KEY, algorithm=ALGORITHM)
     return token_jwt
@@ -24,9 +24,11 @@ def criar_token(dados: dict):
 def verificar_token(token: str):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        crm = payload.get("crm")
-        if crm is None:
+        
+        if payload.get("sub") is None:
             raise HTTPException(status_code=401, detail="Token inválido ou expirado")
+        
         return payload
+    
     except JWTError:
         raise HTTPException(status_code=401, detail="Token inválido ou expirado")
